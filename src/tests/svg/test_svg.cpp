d7e8db85422556d0f4cd6a80f11f265a90329ef6
Richard Lee <rlee@tokyo3.com>
2016-01-10 17:45:13 -0800

    Tied up loose ends with SVG library. This should now generate proper SVGs

--- src/tests/svg/test_svg.cpp
+++ src/tests/svg/test_svg.cpp
@@ -3,13 +3,63 @@
 
 namespace svg {
 
+    struct TestData {
+        Shape *s;
+        std::string e;
+
+        void execute(SVGSVGTest *t) {
+            std::stringstream out;
+
+            this->s->append(out);
+            std::string s = out.str();
+            t->assertEqual("", this->e, s);
+        }
+    };
+
+    const Point triangle[] = {
+        { 1, 1 },
+        { -1, 1 },
+        { 1, -1 }
+    };
+
+    Shape *shapes[] = {
+        new Circle(1, 3, 5)
+    };
+
+    TestData cases[] = {
+        {
+            new Rectangle(1, 3, 5, 7),
+            "<rect x=\"1\" y=\"3\" width=\"5\" height=\"7\" stroke=\"black\" stroke-width=\"2\"/>"
+        },
+        {
+            new Line(8, 9, -3, -5),
+            "<line x1=\"8\" y1=\"9\" x2=\"-3\" y2=\"-5\" stroke=\"black\" stroke-width=\"2\"/>"
+        },
+        {
+            new Circle(5, 6, 13),
+            "<circle cx=\"5\" cy=\"6\" r=\"13\" stroke=\"black\" stroke-width=\"2\"/>"
+        },
+        {
+            new Polyline(triangle, 3),
+            "<polyline points=\"1,1 -1,1 1,-1\" stroke=\"black\" stroke-width=\"2\"/>"
+        },
+        {
+            new SVG(shapes, 1),
+            "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n<circle cx=\"1\" cy=\"3\" r=\"5\" stroke=\"black\" stroke-width=\"2\"/>\n</svg>"
+        },
+        {
+            new SVG("SVG Test", "Testing my SVGs", 640, 480, shapes, 1),
+            "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg width=\"640px\" height=\"480px\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n<title>SVG Test</title>\n<desc>Testing my SVGs</desc>\n<circle cx=\"1\" cy=\"3\" r=\"5\" stroke=\"black\" stroke-width=\"2\"/>\n</svg>"
+        }
+    };
+
+
     void SVGSVGTest::run() {
-        std::stringstream out;
-        Rectangle r(1, 3, 5, 7);
-        r.append(out);
+
+        for (TestData t : cases) {
+            t.execute(this);
+        }
         
-        std::string s = out.str();
-        this->assertEqual("Verify Rectangle", "<rect x=\"1\" y=\"3\" width=\"5\" height=\"7\" stroke=\"black\" stroke-width=\"2\"/>", s);
     }
 
 }
