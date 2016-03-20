7525c6f54f9ee460db06cafe2f647497a87f055b
Richard Lee <rlee@tokyo3.com>
2016-03-19 19:33:24 -0700

    fix svg tests. also added a test for embedded png images in svgs

--- src/svg/svg.cpp
+++ src/svg/svg.cpp
@@ -22,7 +22,7 @@ namespace bust::svg {
         }
 
         stream << "version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" " <<
-                  "xmlns:xlink=\"http://www.w3.org/1999/xlink\" " << 
+                  "xmlns:xlink=\"http://www.w3.org/1999/xlink\"" << 
                   ">" << std::endl;
 
         if (this->title != "") {
--- src/tests/svg/test_svg.cpp
+++ src/tests/svg/test_svg.cpp
@@ -1,5 +1,6 @@
 #include "test_svg.h"
 #include <sstream>
+#include "png.h"
 
 namespace bust::svg {
 
@@ -22,8 +23,21 @@ namespace bust::svg {
         { 1, -1 }
     };
 
+    class RGBPNG1x3 : public bust::png::PNG {
+        public:
+            RGBPNG1x3() : PNG(1, 3) {
+                set(0, 0, (uint32_t) 0xFF0000FF);
+                set(0, 1, (uint32_t) 0x00FF00FF);
+                set(0, 2, (uint32_t) 0x0000FFFF);
+            }
+    } rgb_png_1x3;
+
     Shape *shapes[] = {
         new Circle(1, 3, 5)
+    };
+
+    Shape *images[] = {
+        new Image(1, 3, rgb_png_1x3)
     };
 
     TestData cases[] = {
@@ -45,11 +59,15 @@ namespace bust::svg {
         },
         {
             new SVG(shapes, 1),
-            "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n<circle cx=\"1\" cy=\"3\" r=\"5\" stroke=\"black\" stroke-width=\"2\"/>\n</svg>"
+            "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n<circle cx=\"1\" cy=\"3\" r=\"5\" stroke=\"black\" stroke-width=\"2\"/>\n</svg>"
         },
         {
             new SVG("SVG Test", "Testing my SVGs", 640, 480, shapes, 1),
-            "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg width=\"640px\" height=\"480px\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n<title>SVG Test</title>\n<desc>Testing my SVGs</desc>\n<circle cx=\"1\" cy=\"3\" r=\"5\" stroke=\"black\" stroke-width=\"2\"/>\n</svg>"
+            "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg width=\"640px\" height=\"480px\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n<title>SVG Test</title>\n<desc>Testing my SVGs</desc>\n<circle cx=\"1\" cy=\"3\" r=\"5\" stroke=\"black\" stroke-width=\"2\"/>\n</svg>"
+        },
+        {
+            new SVG(images, 1),
+            "<?xml version=\"1.0\" standalone=\"no\"?>\n<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n<image x=\"1\" y=\"3\" width=\"1\" height=\"3\" xlink:href=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAADCAYAAABS3WWCAAAAGklEQVR4AQEPAPD/AP8AAP8AAP8A/wAAAP//KeUF+7mCYzsAAAAASUVORK5CYII=\" />\n</svg>"
         }
     };
 
